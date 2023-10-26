b96d50ff063feca2619df909a232312abbd14566
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-10-25 22:33:06 -0400

    Update drop_tables.sh

--- drop_tables.sh
+++ drop_tables.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
-sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl12c)))" <<EOF
+sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle12c.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl12c)))" <<EOF
 
 DROP TABLE MEMBERSHIP_LOG;
 DROP TABLE REFEREEING_LOG;
