d6a2a9154804ec83838a5e8c6b39c11221d94528
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2023-10-25 23:13:34 -0400

    Update create_tables.sh

--- create_tables.sh
+++ create_tables.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64 "username/passwword@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle12c.cs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl12c)))" <<EOF
+set sqlblanklines on
 /*This is the start of table creations*/
 CREATE TABLE SEASONS(
     Season_Name varchar(255) NOT NULL PRIMARY KEY,
